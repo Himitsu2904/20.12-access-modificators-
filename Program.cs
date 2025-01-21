1aa0049b1c9bfa6e0c1d8bbcc328fd34102449ab
Oleksandra Chernenko <alexandrachernenko2006@gmail.com>
2025-01-21 09:18:44 +0200

    Созданы классы "Журнал", "Магазин" и "Список книг для прочтения".

--- Program.cs
+++ Program.cs
@@ -4,20 +4,80 @@
     {
         Website web = new Website();
         web.ShowData();
-        Console.Write("Change website's name: ");
-        web.Name = Console.ReadLine();
-        Console.WriteLine("New website's name = " + web.Name);
 
-        Journal jour = new Journal();
-        jour.ShowData();
-        Console.Write("Change name: ");
-        jour.Name = Console.ReadLine();
-        Console.WriteLine("New journal's name = " + jour.Name + ", found in " + jour.FoundationYear);
+        Journal jour1 = new Journal();
+        Journal jour2 = new Journal();
+        jour2 = 22 + jour1;
+        Console.WriteLine($"\n22 + jour1({jour1.EmployeesCount}) = jour2({jour2.EmployeesCount})");
+        Console.Write($"jour1({jour1.EmployeesCount}) + 4 = ");
+        jour1 += 4;
+        Console.WriteLine($"jour1({jour1.EmployeesCount})");
+        Console.Write($"jour2({jour2.EmployeesCount}) - 54 = ");
+        try
+        {
+            jour2 -= 54;
+        }
+        catch (BelowZeroException ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(ex.Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine($"jour2({jour2.EmployeesCount})");
+        Console.WriteLine($"17 = jour1({jour1.EmployeesCount}): {17 == jour1}");
+        Console.WriteLine($"8 != jour1({jour1.EmployeesCount}): {8 != jour1}");
+        Console.WriteLine($"jour2({jour2.EmployeesCount}) = 26: {jour2 == 26}");
+        Console.WriteLine($"jour2({jour2.EmployeesCount}) != 49: {jour2 != 49}");
+        Console.WriteLine($"jour1({jour1.EmployeesCount}) = jour2({jour2.EmployeesCount}): {jour1 == jour2}");
+        Console.WriteLine($"jour1({jour1.EmployeesCount}) != jour2({jour2.EmployeesCount}): {jour1 != jour2}");
+        Console.WriteLine($"17 < jour1({jour1.EmployeesCount}): {17 < jour1}");
+        Console.WriteLine($"8 > jour1({jour1.EmployeesCount}): {8 > jour1}");
+        Console.WriteLine($"jour2({jour2.EmployeesCount}) < 26: {jour2 < 26}");
+        Console.WriteLine($"jour2({jour2.EmployeesCount}) > 49: {jour2 > 49}");
+        Console.WriteLine($"jour1({jour1.EmployeesCount}) < jour2({jour2.EmployeesCount}): {jour1 < jour2}");
+        Console.WriteLine($"jour1({jour1.EmployeesCount}) > jour2({jour2.EmployeesCount}): {jour1 > jour2}");
+        Console.WriteLine($"jour1.Equals(jour2): {jour1.Equals(jour2)}");
 
-        Shop shop = new Shop();
-        shop.ShowData();
-        Console.Write("Change shop's address: ");
-        shop.Address = Console.ReadLine();
-        Console.WriteLine("New shop's address = " + shop.Address);
+        Shop shop1 = new Shop();
+        Shop shop2 = new Shop();
+        shop2 = 22 + shop1;
+        Console.WriteLine($"\n22 + shop1({shop1.Area}) = shop2({shop2.Area})");
+        Console.Write($"shop1({shop1.Area}) + 4 = ");
+        shop1 += 4;
+        Console.WriteLine($"shop1({shop1.Area})");
+        Console.Write($"shop2({shop2.Area}) - 54 = ");
+        try
+        {
+            shop2 -= 54;
+        }
+        catch (BelowZeroException ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(ex.Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine($"shop2({shop2.Area})");
+        Console.WriteLine($"shop1({shop1.Area}) = shop2({shop2.Area}): {shop1 == shop2}");
+        Console.WriteLine($"shop1({shop1.Area}) != shop2({shop2.Area}): {shop1 != shop2}");
+        Console.WriteLine($"17 < shop1({shop1.Area}): {17 < shop1}");
+        Console.WriteLine($"8 > shop1({shop1.Area}): {8 > shop1}");
+        Console.WriteLine($"shop2({shop2.Area}) < 26: {shop2 < 26}");
+        Console.WriteLine($"shop2({shop2.Area}) > 49: {shop2 > 49}");
+        Console.WriteLine($"shop1({shop1.Area}) < shop2({shop2.Area}): {shop1 < shop2}");
+        Console.WriteLine($"shop1({shop1.Area}) > shop2({shop2.Area}): {shop1 > shop2}");
+        Console.WriteLine($"shop1.Equals(shop2): {shop1.Equals(shop2)}");
+
+        ReadingList list = new ReadingList("My Reading list.");
+        list.ShowList();
+        Book b1 = new Book("Harry Potter", "J. K. Rowling", ReadingStatus.NotStarted);
+        list += b1;
+        Book b2 = new Book("Hamlet", "William Shakespeare", ReadingStatus.InProgress);
+        list += b2;
+        Console.WriteLine();
+        list.ShowList();
+        list -= b1;
+        list.ShowList();
+        Console.WriteLine("\"War and peace\" is present: " + list.Contains("War and peace"));
+        Console.WriteLine("Indexer test: " + list[0].Author);
     }
 }
